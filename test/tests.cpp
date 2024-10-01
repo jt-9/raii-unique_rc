106aa98f9edd1563838063ad1471bf4775398d7c
jt-9 <26926933+jt-9@users.noreply.github.com>
2024-10-02 01:35:11 +0300

    Applied clang-format

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,13 +1,13 @@
 #include <catch2/catch_test_macros.hpp>
 
 
-#include "unique_rc.hpp"
 #include "memory_deleter.hpp"
+#include "unique_rc.hpp"
 
 
 TEST_CASE("Default initialised unique_rc<char*, memory_delete<char*>> default", "[unique_rc]")
 {
-  const raii::unique_rc<char*, raii::memory_delete<char*>> default_init_rc{};
+  const raii::unique_rc<char *, raii::memory_delete<char *>> default_init_rc{};
 
   REQUIRE(default_init_rc.get() == nullptr);
   REQUIRE_FALSE(default_init_rc);
--- unique_rc/inc/deleter/stdio_closer.hpp
+++ unique_rc/inc/deleter/stdio_closer.hpp
@@ -1,6 +1,5 @@
-// memory_delete for unique_rc
-#ifndef STDIO_DELETER_HPP
-#define STDIO_DELETER_HPP
+#ifndef STDIO_CLOSER_HPP
+#define STDIO_CLOSER_HPP
 
 #include "defs.hpp"
 
@@ -10,19 +9,16 @@
 
 RAII_NS_BEGIN
 
-// Calls fclose() which flushes the stream pointed to by h and closes the underlying file descriptor. 
+// Calls fclose() which flushes the stream pointed to by h and closes the underlying file descriptor.
 struct stdio_fclose
 {
   constexpr stdio_fclose() noexcept = default;
 
   [[nodiscard]] raii_inline static constexpr std::nullptr_t invalid() noexcept { return nullptr; }
 
-  raii_inline void operator()(FILE* h) const noexcept
-  {
-    fclose(h);
-  }
+  raii_inline void operator()(FILE *h) const noexcept { fclose(h); }
 };
 
 RAII_NS_END
 
-#endif// STDIO_DELETER_HPP
+#endif// STDIO_CLOSER_HPP
