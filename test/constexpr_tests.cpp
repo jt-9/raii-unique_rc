5564d83cfabd4dd8d5dbdce52709899ef7b00164
jt-9 <26926933+jt-9@users.noreply.github.com>
2024-10-03 01:35:11 +0300

    Fixes constraints and adds tests for == and <=>

--- test/constexpr_tests.cpp
+++ test/constexpr_tests.cpp
@@ -3,11 +3,35 @@
 #include "memory_deleter.hpp"
 #include "unique_rc.hpp"
 
-TEST_CASE("Default initialised unique_rc<char*, memory_delete<char*>> default", "[unique_rc]")
+TEST_CASE("Default initialised unique_rc<char*, memory_delete<char*>>", "[unique_rc::unique_rc()]")
 {
-  constexpr raii::unique_rc<char *, raii::memory_delete<char *>> default_init_rc{};
+  constexpr raii::unique_rc<char *, raii::memory_delete<char *>> char_rc{};
 
-  STATIC_REQUIRE(default_init_rc.get() == nullptr);
-  STATIC_REQUIRE_FALSE(default_init_rc);
-  // STATIC_REQUIRE(factorial_constexpr(10) == 3628800);
+  STATIC_REQUIRE(char_rc.get() == nullptr);
+  STATIC_REQUIRE_FALSE(char_rc);
+}
+
+TEST_CASE("invalid() of default initialised unique_rc<char*, memory_delete<char*>>", "[unique_rc::invalid()]")
+{
+  constexpr raii::unique_rc<char *, raii::memory_delete<char *>> char_rc{};
+
+  STATIC_REQUIRE(decltype(char_rc)::invalid() == nullptr);
+}
+
+TEST_CASE("Equality between default initialised unique_rc<int*, memory_delete<int*>>",
+  "[unique_rc::operator ==(unique_rc)]")
+{
+  constexpr raii::unique_rc<int *, raii::memory_delete<int *>> int_rc1{};
+  constexpr raii::unique_rc<int *, raii::memory_delete<int *>> int_rc2{};
+
+  STATIC_REQUIRE(int_rc1 == int_rc2);
+}
+
+TEST_CASE("Three-way comparison between default initialised unique_rc<int*, memory_delete<int*>>",
+  "[unique_rc::operator <=>(unique_rc)]")
+{
+  constexpr raii::unique_rc<int *, raii::memory_delete<int *>> int_rc1{};
+  constexpr raii::unique_rc<int *, raii::memory_delete<int *>> int_rc2{};
+
+  STATIC_REQUIRE((int_rc1 <=> int_rc2) == std::strong_ordering::equal);
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -5,11 +5,29 @@
 #include "unique_rc.hpp"
 
 
-TEST_CASE("Default initialised unique_rc<char*, memory_delete<char*>> default", "[unique_rc]")
+TEST_CASE("Default initialised unique_rc<char*, memory_delete<char*>> default", "[unique_rc::unique_rc()]")
 {
-  const raii::unique_rc<char *, raii::memory_delete<char *>> default_init_rc{};
+  const raii::unique_rc<char *, raii::memory_delete<char *>> char_rc{};
 
-  REQUIRE(default_init_rc.get() == nullptr);
-  REQUIRE_FALSE(default_init_rc);
+  REQUIRE(char_rc.get() == nullptr);
+  REQUIRE_FALSE(char_rc);
   // REQUIRE(factorial(10) == 3628800);
+}
+
+TEST_CASE("Default initialised unique_rc<char*, memory_delete<char*>> default", "[unique_rc::release()]")
+{
+  raii::unique_rc<char *, raii::memory_delete<char *>> char_rc{};
+
+  REQUIRE(char_rc.release() == nullptr);
+}
+
+TEST_CASE("Initialised unique_rc<char*, memory_delete<char*>> default", "[unique_rc::unique_rc(new char)]")
+{
+  raii::unique_rc<char *, raii::memory_delete<char *>> char_rc{ new char{ 'A' } };
+
+  CHECK(char_rc.get() != nullptr);
+  CHECK(char_rc);
+  CHECK_NOTHROW(char_rc.reset());
+  CHECK_FALSE(char_rc);
+  REQUIRE(char_rc.release() == nullptr);
 }
