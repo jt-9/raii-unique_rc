bc897a9ab60b96e205e4288352a991eac5ee532b
jt-9 <26926933+jt-9@users.noreply.github.com>
2024-10-03 21:34:59 +0300

    Adds no-op memory_mock_deleter, more tests

--- test/constexpr_tests.cpp
+++ test/constexpr_tests.cpp
@@ -3,6 +3,9 @@
 #include "memory_deleter.hpp"
 #include "unique_rc.hpp"
 
+#include "memory_mock_deleter.hpp"
+
+#include <string_view>
 
 TEST_CASE("Default initialised unique_rc<char*, memory_delete<char*>>", "[unique_rc]")
 {
@@ -12,6 +15,7 @@ TEST_CASE("Default initialised unique_rc<char*, memory_delete<char*>>", "[unique
   {
     STATIC_REQUIRE(char_rc.get() == nullptr);
     STATIC_REQUIRE_FALSE(char_rc);
+    STATIC_REQUIRE(char_rc == nullptr);
   }
 
   SECTION("unique_rc::invalid()") { STATIC_REQUIRE(decltype(char_rc)::invalid() == nullptr); }
@@ -55,5 +59,62 @@ TEST_CASE("Three-way comparison of default initialised unique_rc<int*, memory_de
   SECTION("unique_rc::operator <=>(std::nullptr_t, unique_rc)")
   {
     STATIC_REQUIRE((nullptr <=> int_rc2) == std::strong_ordering::equal);
+  }
+}
+
+TEST_CASE("Pointer initialised unique_rc<const char*, memory_mock_delete<const char*>>", "[unique_rc]")
+{
+  using namespace std::literals;
+
+  constexpr auto str = "This is a test string"sv;
+
+  // NOLINTBEGIN(bugprone-suspicious-stringview-data-usage)
+  constexpr raii::unique_rc<const char *, mock_raii::memor_mock_delete<const char *>> char_rc{ str.data() };
+  // NOLINTEND(bugprone-suspicious-stringview-data-usage)
+
+  STATIC_REQUIRE(char_rc.get() != nullptr);
+  STATIC_REQUIRE(char_rc.get() == str.data());
+}
+
+TEST_CASE("Equality of initialised unique_rc<const char*, memory_mock_delete<const char*>>", "[unique_rc::operator ==]")
+{
+  using namespace std::literals;
+
+  constexpr auto str = "This is a test string"sv;
+
+  // NOLINTBEGIN(bugprone-suspicious-stringview-data-usage)
+  constexpr raii::unique_rc<const char *, mock_raii::memor_mock_delete<const char *>> char_rc1{ str.data() };
+  constexpr raii::unique_rc<const char *, mock_raii::memor_mock_delete<const char *>> char_rc2{ str.data() + 1 };
+  // NOLINTEND(bugprone-suspicious-stringview-data-usage)
+
+  SECTION("unique_rc::operator ==(unique_rc, unique_rc)")
+  {
+    STATIC_CHECK(char_rc1);
+    STATIC_CHECK(char_rc2);
+    STATIC_REQUIRE(char_rc1 != char_rc2);
+  }
+
+  SECTION("unique_rc::operator ==(unique_rc, std::nullptr_t)") { STATIC_REQUIRE(char_rc1 != nullptr); }
+
+  SECTION("unique_rc::operator ==(std::nullptr_t, unique_rc)") { STATIC_REQUIRE(nullptr != char_rc2); }
+}
+
+TEST_CASE("Three-way comparison of initialised unique_rc<const char*, memory_mock_delete<const char*>>", "[unique_rc::operator <=>]")
+{
+  using namespace std::literals;
+
+  constexpr auto str = "This is a test string"sv;
+
+  // NOLINTBEGIN(bugprone-suspicious-stringview-data-usage)
+  constexpr raii::unique_rc<const char *, mock_raii::memor_mock_delete<const char *>> char_rc1{ str.data() };
+  constexpr raii::unique_rc<const char *, mock_raii::memor_mock_delete<const char *>> char_rc2{ str.data() + 4 };
+  // NOLINTEND(bugprone-suspicious-stringview-data-usage)
+
+  SECTION("unique_rc::operator <=>(unique_rc, unique_rc)")
+  {
+    STATIC_CHECK(char_rc1);
+    STATIC_CHECK(char_rc2);
+    STATIC_REQUIRE((char_rc1 <=> char_rc2) == std::strong_ordering::less);
+    STATIC_REQUIRE((char_rc2 <=> char_rc1) == std::strong_ordering::greater);
   }
 }
