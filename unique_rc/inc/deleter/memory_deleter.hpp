8326b4d89548702155677a641f74ec21f6a049f6
jt-9 <26926933+jt-9@users.noreply.github.com>
2024-10-04 19:39:42 +0300

    Adds tests to unique_rc::reset() and unique_rc::release()

--- unique_rc/inc/deleter/memory_deleter.hpp
+++ unique_rc/inc/deleter/memory_deleter.hpp
@@ -14,21 +14,19 @@ template<typename Handle>
   requires std::is_pointer_v<Handle>
 struct memory_delete
 {
-  using handle = Handle;
-
   constexpr memory_delete() noexcept = default;
 
   template<typename U>
   raii_inline constexpr memory_delete(const memory_delete<U> &) noexcept
-    requires std::is_convertible_v<U, handle>
+    requires std::is_convertible_v<U, Handle>
   {}
 
   [[nodiscard]] raii_inline static constexpr std::nullptr_t invalid() noexcept { return nullptr; }
 
-  raii_inline constexpr void operator()(handle h) const noexcept
+  raii_inline constexpr void operator()(Handle h) const noexcept
   {
-    static_assert(!std::is_void_v<handle>, "can't delete pointer to incomplete type");
-    static_assert(sizeof(handle) > 0, "can't delete pointer to incomplete type");
+    static_assert(!std::is_void_v<Handle>, "can't delete pointer to incomplete type");
+    static_assert(sizeof(Handle) > 0, "can't delete pointer to incomplete type");
 
     delete h;
   }
@@ -38,22 +36,20 @@ struct memory_delete
 // Specialization of default_delete for arrays, used by `unique_rc<T[]>`
 template<typename Handle> struct memory_delete<Handle[]>
 {
-  using handle = Handle;
-
   constexpr memory_delete() noexcept = default;
 
   template<typename U>
   raii_inline constexpr memory_delete(const memory_delete<U[]> &) noexcept
-    requires std::is_convertible_v<U (*)[], handle (*)[]>
+    requires std::is_convertible_v<U (*)[], Handle (*)[]>
   {}
 
   [[nodiscard]] raii_inline static constexpr std::nullptr_t invalid() noexcept { return nullptr; }
 
   template<typename U>
   raii_inline constexpr void operator()(U h) const noexcept
-    requires std::is_convertible_v<U (*)[], handle (*)[]>
+    requires std::is_convertible_v<U (*)[], Handle (*)[]>
   {
-    static_assert(sizeof(handle) > 0, "can't delete pointer to incomplete type");
+    static_assert(sizeof(Handle) > 0, "can't delete pointer to incomplete type");
 
     delete[] h;
   }
