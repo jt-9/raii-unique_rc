463c6244a2821af011b8d0dfd36b527123ea9bea
jt-9 <26926933+jt-9@users.noreply.github.com>
2024-10-07 19:54:52 +0300

    Adds Windows GDI tests. Corrects constexpr for deleters

--- unique_rc/inc/deleter/windows_gdi_deleter.hpp
+++ unique_rc/inc/deleter/windows_gdi_deleter.hpp
@@ -10,58 +10,49 @@
 
 RAII_NS_BEGIN
 
-// Use to release GDI objects pen, brush, bitmaps, etc. nullptr indicates invalid object
+// Deallocates GDI objects such as pen, brush, bitmaps, etc.
+// nullptr indicates invalid object
 template<typename Handle>
   requires std::convertible_to<Handle, HGDIOBJ>
 struct gdi_delete_object_nullptr
 {
-  using handle = Handle;
-
   constexpr gdi_delete_object_nullptr() noexcept = default;
 
   template<typename U>
   raii_inline constexpr gdi_delete_object_nullptr(const gdi_delete_object_nullptr<U> &) noexcept
-    requires std::is_convertible_v<U, handle>
+    requires std::is_convertible_v<U, Handle>
   {}
 
   [[nodiscard]] raii_inline static constexpr std::nullptr_t invalid() noexcept { return nullptr; }
 
-  raii_inline constexpr void operator()(handle h) const noexcept { DeleteObject(h); }
+  raii_inline constexpr void operator()(Handle h) const noexcept { DeleteObject(h); }
 };
 
-// Use to release DC acquired via CreateDC, CreateCompatibleDC, etc. nullptr indicates invalid dc
+// Deallocates device context (DC) acquired via CreateDC, CreateCompatibleDC, etc.
+// nullptr indicates invalid dc
 struct gdi_delete_dc_nullptr
 {
-  using handle = HDC;
-
   constexpr gdi_delete_dc_nullptr() noexcept = default;
 
   [[nodiscard]] raii_inline static constexpr std::nullptr_t invalid() noexcept { return nullptr; }
 
-  raii_inline constexpr void operator()(handle h) const noexcept { DeleteDC(h); }
+  raii_inline constexpr void operator()(HDC h) const noexcept { DeleteDC(h); }
 };
 
+// Deallocates device context (DC) acquired via GetDC, GetWindowDC, etc.
+// nullptr indicates invalid dc
 struct gdi_release_wnd_dc_nullptr
 {
-  using handle = HDC;
-
   raii_inline constexpr gdi_release_wnd_dc_nullptr(HWND w) noexcept : wnd_{ w } {}
 
   [[nodiscard]] raii_inline static constexpr std::nullptr_t invalid() noexcept { return nullptr; }
 
-  raii_inline constexpr void operator()(handle h) const noexcept { ReleaseDC(wnd_, h); }
+  raii_inline constexpr void operator()(HDC h) const noexcept { ReleaseDC(wnd_, h); }
 
 private:
   HWND wnd_;
 };
 
-
-// Can be used for bitmaps, pens, brushes, etc.
-// template< typename Handle = HGDIOBJ >
-// using GDIObjectHandle = UniqueHandle<HGDIOBJPolicy<Handle>>;
-//
-// using UniqueDCHandle = UniqueHandle< UniqueHDCPolicy >;
-// using ViewDCHandle = UniqueHandle< GetHDCPolicy >;
 
 RAII_NS_END
 
