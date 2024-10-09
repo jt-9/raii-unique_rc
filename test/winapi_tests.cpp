4754dd06f457af0b4826b2640758b9cb1d89f402
jt-9 <26926933+jt-9@users.noreply.github.com>
2024-10-09 17:50:02 +0300

    Adds Windows API tests pen, mutex

--- test/winapi_tests.cpp
+++ test/winapi_tests.cpp
@@ -1,18 +1,22 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include "unique_rc.hpp"
+#include "windows_close_handle.hpp"
+#include "windows_dc_deleter.hpp"
 #include "windows_gdi_deleter.hpp"
 
 #include <utility>
 #include <windows.h>
 
+
+// GDI pen tests. type HPEN
+//----------------------------------------------------------------
 TEST_CASE("Default initialised unique_rc<HPEN, gdi_delete_object_nullptr<HPEN>>", "[unique_rc::unique_rc]")
 {
   constexpr raii::unique_rc<HPEN, raii::gdi_delete_object_nullptr<HPEN>> pen_rc{};
 
   REQUIRE(pen_rc.get() == nullptr);
   REQUIRE_FALSE(pen_rc);
-  // REQUIRE(factorial(10) == 3628800);
 }
 
 TEST_CASE("Value initialised unique_rc<HPEN, gdi_delete_object_nullptr<HPEN>>", "[unique_rc::unique_rc]")
@@ -22,3 +26,23 @@ TEST_CASE("Value initialised unique_rc<HPEN, gdi_delete_object_nullptr<HPEN>>", 
   REQUIRE(pen_rc.get() != nullptr);
   REQUIRE(pen_rc);
 }
+//****************************************************************
+
+// Mutex tests. type HANDLE
+//----------------------------------------------------------------
+TEST_CASE("Default initialised unique_rc<HANDLE, close_handle_nullptr<HANDLE>>", "[unique_rc::unique_rc]")
+{
+  constexpr raii::unique_rc<HANDLE, raii::close_handle_nullptr<HANDLE>> mtx{};
+
+  REQUIRE(mtx.get() == nullptr);
+  REQUIRE_FALSE(mtx);
+}
+
+TEST_CASE("Value initialised unique_rc<HANDLE, close_handle_nullptr<HANDLE>>", "[unique_rc::unique_rc]")
+{
+  raii::unique_rc<HANDLE, raii::close_handle_nullptr<HANDLE>> mtx{ CreateMutex(nullptr, FALSE, nullptr) };
+
+  REQUIRE(mtx.get() != nullptr);
+  REQUIRE(mtx);
+}
+//****************************************************************
