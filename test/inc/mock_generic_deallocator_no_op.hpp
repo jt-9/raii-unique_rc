eb8279134e9040f5b460ab6a9d4ee13e548dff87
jt-9 <26926933+jt-9@users.noreply.github.com>
2024-10-10 18:20:05 +0300

    Adds mock_deallocator_no_op which generalises mock deallocator's invalid handle type and value

---
+++ test/inc/mock_generic_deallocator_no_op.hpp
@@ -0,0 +1,26 @@
+#ifndef MOCK_GENERIC_DEALLOCATOR_NO_OP_HPP
+#define MOCK_GENERIC_DEALLOCATOR_NO_OP_HPP
+
+#include <concepts>
+
+namespace mock_raii {
+
+template<typename Handle, typename InvalidHandle, InvalidHandle invalid_value>
+  requires std::is_convertible_v<InvalidHandle, Handle>
+struct mock_deallocator_no_op
+{
+  constexpr mock_deallocator_no_op() noexcept = default;
+
+  template<typename U>
+  raii_inline constexpr mock_deallocator_no_op(const mock_deallocator_no_op<U, InvalidHandle, invalid_value> &) noexcept
+    requires std::is_convertible_v<U, Handle>
+  {}
+
+  [[nodiscard]] raii_inline static constexpr InvalidHandle invalid() noexcept { return invalid_value; }
+
+  raii_inline constexpr void operator()(Handle) const noexcept { /* No op */ }
+};
+
+}// namespace mock_raii
+
+#endif// MOCK_GENERIC_DEALLOCATOR_NO_OP_HPP
--- test/inc/mock_pointer_no_op.hpp
+++ test/inc/mock_pointer_no_op.hpp
@@ -1,6 +1,8 @@
 #ifndef MOCK_POINTER_NO_OP_HPP
 #define MOCK_POINTER_NO_OP_HPP
 
+#include "mock_generic_deallocator_no_op.hpp"
+
 #include <concepts>
 #include <cstddef>
 
@@ -8,19 +10,8 @@ namespace mock_raii {
 
 template<typename Handle>
   requires std::is_pointer_v<Handle>
-struct mock_pointer_no_op
-{
-  constexpr mock_pointer_no_op() noexcept = default;
+using mock_pointer_no_op = mock_deallocator_no_op<Handle, std::nullptr_t, nullptr>;
 
-  template<typename U>
-  raii_inline constexpr mock_pointer_no_op(const mock_pointer_no_op<U> &) noexcept
-    requires std::is_convertible_v<U, Handle>
-  {}
-
-  [[nodiscard]] raii_inline static constexpr std::nullptr_t invalid() noexcept { return nullptr; }
-
-  raii_inline constexpr void operator()(Handle) const noexcept { /* No op */ }
-};
 
 }// namespace mock_raii
 
