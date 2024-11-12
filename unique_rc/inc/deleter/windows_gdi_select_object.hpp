aaf034e4fc224264c8ef8d2f4b00cd08b3e9cd23
jt-9 <26926933+jt-9@users.noreply.github.com>
2024-11-12 21:19:24 +0200

    Adds explicit to one parameter deleter's constructor, converting move constructor to gdi_select_object_nullptr

--- unique_rc/inc/deleter/windows_gdi_select_object.hpp
+++ unique_rc/inc/deleter/windows_gdi_select_object.hpp
@@ -5,7 +5,9 @@
 
 #include <concepts>
 #include <cstddef>
-#include <windows.h>
+#include <utility>
+
+#include <Windows.h>
 
 
 RAII_NS_BEGIN
@@ -16,16 +18,32 @@ template<typename Handle>
   requires std::convertible_to<Handle, HGDIOBJ>
 struct gdi_select_object_nullptr
 {
-  constexpr gdi_select_object_nullptr(HDC hdc) noexcept : hdc_{ hdc } {}
+  raii_inline explicit constexpr gdi_select_object_nullptr(HDC hdc) noexcept : hdc_{ hdc } {}
 
   template<typename U>
-  raii_inline constexpr gdi_select_object_nullptr(const gdi_select_object_nullptr<U> &) noexcept
+  raii_inline constexpr gdi_select_object_nullptr(const gdi_select_object_nullptr<U> &src) noexcept
     requires std::is_convertible_v<U, Handle>
+    : hdc_{ src.hdc_ }
+  {}
+
+  template<typename U>
+  raii_inline constexpr gdi_select_object_nullptr(gdi_select_object_nullptr<U> &&src) noexcept
+    requires std::is_convertible_v<U, Handle>
+    : hdc_{ std::move(src.release_hdc()) }
   {}
 
   [[nodiscard]] raii_inline static constexpr std::nullptr_t invalid() noexcept { return nullptr; }
 
   raii_inline constexpr void operator()(Handle h) const noexcept { SelectObject(hdc_, h); }
+
+private:
+  raii_inline constexpr HDC release_hdc() const noexcept
+  {
+    const auto old_hdc = hdc_;
+    hdc_ = nullptr;
+
+    return old_hdc;
+  }
 
 private:
   HDC hdc_;
