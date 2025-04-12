3715d8c7bf9551e5d939b456bbd730f9006bd855
jt-9 <26926933+jt-9@users.noreply.github.com>
2025-04-12 03:06:18 +0300

    Moves LWG 2762, DR 2899 into .cpp files

---
+++ test/src/requirements/lwg2762.cpp
@@ -0,0 +1,48 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "memory_delete.hpp"
+#include "unique_ptr.hpp"
+#include "unique_rc.hpp"
+
+#include <utility>//std::declval
+
+template<bool B> struct TestDeleter
+{
+  struct pointer
+  {
+    int &operator*() && noexcept(B);// this is used by unique_ptr
+    int &operator*() const & = delete;// this should not be
+
+    int *operator->() noexcept(false);// noexcept here doesn't affect anything
+
+    // Needed for NullablePointer requirements
+    explicit pointer(int * = nullptr);
+    bool operator==(const pointer &) const noexcept;
+    bool operator!=(const pointer &) const noexcept;
+  };
+
+  void operator()([[maybe_unused]] pointer ptr) const noexcept {}
+};
+
+template<typename T, bool Nothrow> using UPtr = raii::unique_ptr<T, raii::deleter_wrapper<TestDeleter<Nothrow>>>;
+
+TEST_CASE("LWG 2762 unique_ptr operator*() should be noexcept", "[unique_ptr::operator *]")
+{
+  // 2762. unique_ptr operator*() should be noexcept
+  STATIC_CHECK(noexcept(*std::declval<raii::unique_ptr<long>>()));
+
+  // noexcept-specifier depends on the pointer type
+  STATIC_CHECK(noexcept(*std::declval<UPtr<int, true> &>()));
+  STATIC_CHECK_FALSE(noexcept(*std::declval<UPtr<int, false> &>()));
+
+  // This has always been required, even in C++11.
+  STATIC_REQUIRE(noexcept(std::declval<raii::unique_ptr<long>>().operator->()));
+  STATIC_REQUIRE(noexcept(std::declval<UPtr<int, false> &>().operator->()));
+}
+
+TEST_CASE("LWG 2762 unique_rc operator->() should be noexcept", "[unique_rc::operator ->]")
+{
+  STATIC_REQUIRE(noexcept(std::declval<raii::unique_rc<long *, raii::default_delete<long>>>().operator->()));
+  STATIC_REQUIRE(
+    noexcept(std::declval<raii::unique_rc<int *, raii::deleter_wrapper<TestDeleter<false>>> &>().operator->()));
+}
