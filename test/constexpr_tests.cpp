4db2eb1f4e48b50b6affa0a8c20995fbdd7b171d
jt-9 <26926933+jt-9@users.noreply.github.com>
2025-04-12 03:06:18 +0300

    Moves LWG 2762, DR 2899 into .cpp files

--- test/constexpr_tests.cpp
+++ test/constexpr_tests.cpp
@@ -1,15 +1,11 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include "memory_delete.hpp"
-#include "unique_ptr.hpp"
 #include "unique_rc.hpp"
 
 #include "mock_pointer_no_op.hpp"
 
-#include <cstddef>
 #include <string_view>
-#include <type_traits>
-#include <utility>
 
 
 TEST_CASE("Default initialised unique_rc<char*, memory_delete<char*>>", "[unique_rc]")
@@ -124,120 +120,5 @@ TEST_CASE("Three-way comparison of initialised unique_rc<const char*, memory_moc
     STATIC_REQUIRE((char_rc1 <=> char_rc2) == std::strong_ordering::less);
     STATIC_REQUIRE((char_rc2 <=> char_rc1) == std::strong_ordering::greater);
     STATIC_REQUIRE((char_rc1 <=> copy_rc1) == std::strong_ordering::equal);
-  }
-}
-
-template<bool B> struct TestDeleter
-{
-  struct pointer
-  {
-    int &operator*() && noexcept(B);// this is used by unique_ptr
-    int &operator*() const & = delete;// this should not be
-
-    int *operator->() noexcept(false);// noexcept here doesn't affect anything
-
-    // Needed for NullablePointer requirements
-    explicit pointer(int * = nullptr);
-    bool operator==(const pointer &) const noexcept;
-    bool operator!=(const pointer &) const noexcept;
-  };
-
-  void operator()([[maybe_unused]] pointer ptr) const noexcept {}
-};
-
-template<typename T, bool Nothrow> using UPtr = raii::unique_ptr<T, raii::deleter_wrapper<TestDeleter<Nothrow>>>;
-
-TEST_CASE("LWG 2762 unique_ptr operator*() should be noexcept", "[unique_ptr::operator *]")
-{
-  // 2762. unique_ptr operator*() should be noexcept
-  STATIC_CHECK(noexcept(*std::declval<raii::unique_ptr<long>>()));
-
-  // noexcept-specifier depends on the pointer type
-  STATIC_CHECK(noexcept(*std::declval<UPtr<int, true> &>()));
-  STATIC_CHECK_FALSE(noexcept(*std::declval<UPtr<int, false> &>()));
-
-  // This has always been required, even in C++11.
-  STATIC_REQUIRE(noexcept(std::declval<raii::unique_ptr<long>>().operator->()));
-  STATIC_REQUIRE(noexcept(std::declval<UPtr<int, false> &>().operator->()));
-}
-
-
-// NOLINTNEXTLINE(readability-function-cognitive-complexity)
-TEST_CASE("DR 2899 is_(nothrow_)move_constructible unique_rc", "[unique_rc::unique_rc]")
-{
-  SECTION("is_(nothrow_)move_constructible unique_rc")
-  {
-    // NOLINTBEGIN(cppcoreguidelines-special-member-functions, hicpp-special-member-functions)
-    struct Del
-    {
-      static constexpr std::nullptr_t invalid() noexcept;
-      static constexpr bool is_owned(int *) noexcept;
-
-      Del() = default;
-      ~Del() = default;
-
-      Del(Del &&) = delete;
-
-      void operator()(int *) const;
-    };
-    // NOLINTEND(cppcoreguidelines-special-member-functions, hicpp-special-member-functions)
-
-    STATIC_REQUIRE_FALSE(std::is_move_constructible_v<raii::unique_rc<int *, Del>>);
-    STATIC_REQUIRE(std::is_move_constructible_v<raii::unique_rc<int *, Del &>>);
-  }
-
-  SECTION("is_(nothrow_)move_assignable unique_rc p2")
-  {
-    // NOLINTBEGIN(cppcoreguidelines-special-member-functions, hicpp-special-member-functions)
-    struct Del2
-    {
-      static constexpr std::nullptr_t invalid() noexcept;
-      static constexpr bool is_owned(int *) noexcept;
-
-      Del2() = default;
-      ~Del2() = default;
-
-      Del2(Del2 &&) = default;
-      Del2 &operator=(Del2 &&) = delete;
-      Del2 &operator=(const Del2 &) = default;
-
-      void operator()(int *) const;
-    };
-    // NOLINTEND(cppcoreguidelines-special-member-functions, hicpp-special-member-functions)
-
-    STATIC_REQUIRE_FALSE(std::is_move_assignable_v<raii::unique_rc<int*, Del2>>);
-    STATIC_REQUIRE_FALSE(std::is_move_assignable_v<raii::unique_ptr<int, Del2>>);
-
-    STATIC_REQUIRE(std::is_move_assignable_v<raii::unique_rc<int*, Del2 &>>);
-    STATIC_REQUIRE(std::is_move_assignable_v<raii::unique_ptr<int, Del2 &>>);
-    // STATIC_REQUIRE_FALSE(std::is_move_assignable_v<raii::deleter_wrapper<Del2>>);
-    //  STATIC_REQUIRE_FALSE(std::is_move_assignable_v<raii::unique_ptr<int, raii::deleter_wrapper<Del2>>>);
-    // STATIC_REQUIRE(std::is_move_assignable_v<raii::unique_ptr<int, raii::deleter_wrapper<Del2> &>>);
-  }
-
-  SECTION("is_(nothrow_)move_assignable unique_rc p3")
-  {
-    // NOLINTBEGIN(cppcoreguidelines-special-member-functions, hicpp-special-member-functions)
-    struct Del3
-    {
-      static constexpr std::nullptr_t invalid() noexcept;
-      static constexpr bool is_owned(int *) noexcept;
-
-      Del3() = default;
-      ~Del3() = default;
-
-      Del3(Del3 &&) = default;
-      Del3 &operator=(Del3 &&) = default;
-      Del3 &operator=(const Del3 &) = delete;
-
-      void operator()(int *) const;
-    };
-    // NOLINTEND(cppcoreguidelines-special-member-functions, hicpp-special-member-functions)
-
-    STATIC_REQUIRE(std::is_move_assignable_v<raii::unique_rc<int*, Del3>>);
-    STATIC_REQUIRE(std::is_move_assignable_v<raii::unique_ptr<int, Del3>>);
-
-    STATIC_REQUIRE_FALSE(std::is_move_assignable_v<raii::unique_rc<int*, Del3 &>>);
-    STATIC_REQUIRE_FALSE(std::is_move_assignable_v<raii::unique_ptr<int, Del3 &>>);
   }
 }
---
+++ test/src/requirements/dr2899.cpp
@@ -0,0 +1,81 @@
+#include <catch2/catch_test_macros.hpp>
+
+//#include "memory_delete.hpp"
+#include "unique_ptr.hpp"
+#include "unique_rc.hpp"
+
+#include <cstddef>//std::nullptr_t
+#include <type_traits>
+
+TEST_CASE("DR 2899 is_(nothrow_)move_constructible unique_rc", "[unique_rc::unique_rc]")
+{
+  // NOLINTBEGIN(cppcoreguidelines-special-member-functions, hicpp-special-member-functions)
+  struct Del
+  {
+    static constexpr std::nullptr_t invalid() noexcept;
+    static constexpr bool is_owned(int *) noexcept;
+
+    Del() = default;
+    ~Del() = default;
+
+    Del(Del &&) = delete;
+
+    void operator()(int *) const;
+  };
+  // NOLINTEND(cppcoreguidelines-special-member-functions, hicpp-special-member-functions)
+
+  STATIC_REQUIRE_FALSE(std::is_move_constructible_v<raii::unique_rc<int *, Del>>);
+  STATIC_REQUIRE(std::is_move_constructible_v<raii::unique_rc<int *, Del &>>);
+}
+
+TEST_CASE("DR 2899 is_(nothrow_)move_assignable unique_rc", "[unique_rc::operator=]")
+{
+  // NOLINTBEGIN(cppcoreguidelines-special-member-functions, hicpp-special-member-functions)
+  struct Del2
+  {
+    static constexpr std::nullptr_t invalid() noexcept;
+    static constexpr bool is_owned(int *) noexcept;
+
+    Del2() = default;
+    ~Del2() = default;
+
+    Del2(Del2 &&) = default;
+    Del2 &operator=(Del2 &&) = delete;
+    Del2 &operator=(const Del2 &) = default;
+
+    void operator()(int *) const;
+  };
+  // NOLINTEND(cppcoreguidelines-special-member-functions, hicpp-special-member-functions)
+
+  STATIC_REQUIRE_FALSE(std::is_move_assignable_v<raii::unique_rc<int *, Del2>>);
+  STATIC_REQUIRE_FALSE(std::is_move_assignable_v<raii::unique_ptr<int, Del2>>);
+
+  STATIC_REQUIRE(std::is_move_assignable_v<raii::unique_rc<int *, Del2 &>>);
+  STATIC_REQUIRE(std::is_move_assignable_v<raii::unique_ptr<int, Del2 &>>);
+  // STATIC_REQUIRE_FALSE(std::is_move_assignable_v<raii::deleter_wrapper<Del2>>);
+  //  STATIC_REQUIRE_FALSE(std::is_move_assignable_v<raii::unique_ptr<int, raii::deleter_wrapper<Del2>>>);
+  // STATIC_REQUIRE(std::is_move_assignable_v<raii::unique_ptr<int, raii::deleter_wrapper<Del2> &>>);
+
+  // NOLINTBEGIN(cppcoreguidelines-special-member-functions, hicpp-special-member-functions)
+  struct Del3
+  {
+    static constexpr std::nullptr_t invalid() noexcept;
+    static constexpr bool is_owned(int *) noexcept;
+
+    Del3() = default;
+    ~Del3() = default;
+
+    Del3(Del3 &&) = default;
+    Del3 &operator=(Del3 &&) = default;
+    Del3 &operator=(const Del3 &) = delete;
+
+    void operator()(int *) const;
+  };
+  // NOLINTEND(cppcoreguidelines-special-member-functions, hicpp-special-member-functions)
+
+  STATIC_REQUIRE(std::is_move_assignable_v<raii::unique_rc<int *, Del3>>);
+  STATIC_REQUIRE(std::is_move_assignable_v<raii::unique_ptr<int, Del3>>);
+
+  STATIC_REQUIRE_FALSE(std::is_move_assignable_v<raii::unique_rc<int *, Del3 &>>);
+  STATIC_REQUIRE_FALSE(std::is_move_assignable_v<raii::unique_ptr<int, Del3 &>>);
+}
