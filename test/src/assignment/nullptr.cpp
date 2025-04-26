a8fb5a3c129c489cfe38e9194e25fe373f8d0006
jt-9 <26926933+jt-9@users.noreply.github.com>
2025-04-26 03:07:05 +0300

    Move-assignment unique_ptr/rc tests

---
+++ test/src/assignment/nullptr.cpp
@@ -0,0 +1,28 @@
+#include <catch2/catch_test_macros.hpp>
+
+
+#include "unique_ptr.hpp"
+// #include "unique_rc.hpp"
+
+namespace {
+struct A
+{
+};
+}// namespace
+
+TEST_CASE("Move assign nullptr to single object unique_ptr", "[unique_ptr][operator=]")
+{
+  raii::unique_ptr<A> ptr_a(new A);
+  CHECK(ptr_a.get() != nullptr);
+  ptr_a = nullptr;
+  REQUIRE(ptr_a.get() == nullptr);
+}
+
+TEST_CASE("Move assign nullptr to array objects unique_ptr", "[unique_ptr][operator=]")
+{
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays,hicpp-avoid-c-arrays,modernize-avoid-c-arrays)
+  raii::unique_ptr<A[]> ptr_aa(new A[2]);
+  CHECK(ptr_aa.get() != nullptr);
+  ptr_aa = nullptr;
+  REQUIRE(ptr_aa.get() == nullptr);
+}
