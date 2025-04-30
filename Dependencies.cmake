1b93c7d28f410fd6f5c86458bdb18c8a3266797e
jt-9 <26926933+jt-9@users.noreply.github.com>
2025-04-30 18:34:44 +0300

    Bumps up catchorg::catch2 to version 3.8.1

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -25,7 +25,7 @@ function(myproject_setup_dependencies)
   # endif()
 
   if(NOT TARGET Catch2::Catch2WithMain)
-    cpmaddpackage("gh:catchorg/Catch2@3.8.0")
+    cpmaddpackage("gh:catchorg/Catch2@3.8.1")
   endif()
 
   if(NOT TARGET CLI11::CLI11)
