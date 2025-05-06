91d681df2df9c7dbe88bcbcb275fc65f48fdf9ba
jt-9 <26926933+jt-9@users.noreply.github.com>
2025-05-06 22:23:24 +0300

    clang-format concepts.hpp

--- unique_rc/inc/concepts.hpp
+++ unique_rc/inc/concepts.hpp
@@ -17,9 +17,7 @@ template<typename T>
 concept has_pointer_type = requires { typename T::pointer; };
 
 template<typename T>
-concept can_form_pointer = requires {
-    std::declval<T*>();
-};
+concept can_form_pointer = requires { std::declval<T *>(); };
 
 RAII_NS_END
 
