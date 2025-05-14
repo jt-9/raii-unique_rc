964f06e147a07c5adea7c077d027a6cbe5cf122f
jt-9 <26926933+jt-9@users.noreply.github.com>
2025-05-14 23:02:29 +0300

    Reverts to c++20 sample app and tests. Deleters use static call operator if compiled with c++23 or higher

--- unique_rc/inc/deleter/stdio_fclose.hpp
+++ unique_rc/inc/deleter/stdio_fclose.hpp
@@ -3,6 +3,7 @@
 
 #include "raii_defs.hpp"
 
+#include <cstddef>
 #include <stdio.h>
 
 
@@ -16,7 +17,16 @@ struct stdio_fclose
   [[nodiscard]] raii_inline static constexpr std::nullptr_t invalid() noexcept { return nullptr; }
   [[nodiscard]] raii_inline static constexpr bool is_owned(FILE *h) noexcept { return h; }
 
-  raii_inline void operator()(FILE *h) const noexcept { fclose(h); }
+#ifdef __cpp_static_call_operator
+  // False poisitive, guarded by feature #ifdef __cpp_static_call_operator
+  // NOLINTNEXTLINE(clang-diagnostic-c++23-extensions)
+  raii_inline static void operator()(FILE *h) noexcept
+#else
+  raii_inline void operator()(FILE *h) const noexcept
+#endif
+  {
+    fclose(h);
+  }
 };
 
 RAII_NS_END
